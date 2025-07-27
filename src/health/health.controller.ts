e082837ae2e8e81a0dd7352b72faf178049248b5
Tomasz Bawor <bawortomasz@gmail.com>
2025-07-27 17:44:51 +0200

    chore: clean

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -2,13 +2,6 @@ import { HttpApiBuilder } from "@effect/platform"
 import { Effect } from "effect"
 import { Api } from "../api.js"
 
-export const HealthGroupLive = HttpApiBuilder.group(Api, "health.api", (handlers) =>
-  Effect.gen(
-    function*() {
-      yield* Effect.logInfo("Health Group is Live")
-
-      return handlers.handle("get", getHealth)
-    }
-  ))
+export const HealthGroupLive = HttpApiBuilder.group(Api, "health.api", (handlers) => handlers.handle("get", getHealth))
 
 const getHealth = () => Effect.succeed("OK")
