a3c7bc46751bc8bd25f3923ea98855da2a96b6fd
Tomasz Bawor <bawortomasz@gmail.com>
2025-07-27 17:28:20 +0200

    chore: minor tinkering

--- src/health/health.api.ts
+++ src/health/health.api.ts
@@ -3,14 +3,14 @@ import { Effect, Schema } from "effect"
 import { Api } from "../api.js"
 
 // Definition
-export class HealthGroup extends HttpApiGroup.make("Health")
+export class HealthGroup extends HttpApiGroup.make("health.api")
   .add(
     HttpApiEndpoint.get("get", "/")
       .addSuccess(Schema.String)
   )
 {}
 
-export const HealthGroupLive = HttpApiBuilder.group(Api, "Health", (handlers) =>
+export const HealthGroupLive = HttpApiBuilder.group(Api, "health.api", (handlers) =>
   Effect.gen(
     function*() {
       yield* Effect.logInfo("Health Group is Live")
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { HttpApiBuilder, HttpServer } from "@effect/platform"
+import { HttpApiBuilder, HttpApiSwagger, HttpMiddleware, HttpServer } from "@effect/platform"
 import { BunHttpServer, BunRuntime } from "@effect/platform-bun"
 import { Layer } from "effect"
 import { Api } from "./api.js"
@@ -8,8 +8,9 @@ const ApiLive = HttpApiBuilder.api(Api).pipe(
   Layer.provide(HealthGroupLive) // Health Endpoints
 )
 
-const HttpLive = HttpApiBuilder.serve().pipe(
+const HttpLive = HttpApiBuilder.serve(HttpMiddleware.logger).pipe(
   HttpServer.withLogAddress, // Logs on what port we are listening too
+  Layer.provide(HttpApiSwagger.layer()),
   Layer.provide(ApiLive),
   Layer.provide(BunHttpServer.layer({
     development: {
