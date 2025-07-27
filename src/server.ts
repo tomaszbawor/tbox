4c9de0ee9f45e27c88cd0fbbc5ecad1b02848ac1
Tomasz Bawor <bawortomasz@gmail.com>
2025-07-27 15:48:58 +0200

    feat: add simple server

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import { HttpApi, HttpApiBuilder, HttpApiEndpoint, HttpApiGroup } from "@effect/platform"
+import { BunHttpServer, BunRuntime } from "@effect/platform-bun"
+import { Effect, Layer, Schema } from "effect"
+
+// api.heath
+class HealthGroup extends HttpApiGroup.make("Health")
+  .add(
+    HttpApiEndpoint.get("get", "/")
+      .addSuccess(Schema.String)
+  )
+{}
+
+const Api = HttpApi.make("Api").add(HealthGroup)
+
+const HealthGroupLive = HttpApiBuilder.group(Api, "Health", (handlers) =>
+  Effect.gen(
+    function*() {
+      yield* Effect.logInfo("Health Group is Live")
+
+      return handlers.handle("get", () => Effect.succeed("OK"))
+    }
+  ))
+
+const ApiLive = HttpApiBuilder.api(Api).pipe(
+  Layer.provide(HealthGroupLive)
+)
+
+const HttpLive = HttpApiBuilder.serve().pipe(
+  Layer.provide(ApiLive),
+  Layer.provide(BunHttpServer.layer({
+    development: {
+      console: true
+    },
+    port: 3333
+  }))
+)
+
+BunRuntime.runMain(Layer.launch(HttpLive))
